09165a6b0ef6ee62b4bddebfe0f4b86f4175d3f9
Tim Aerts <aardbeiplantje@gmail.com>
2024-11-28 22:43:12 +0100

    version from curl.pm

--- lib/utils/curl.pm
+++ lib/utils/curl.pm
@@ -3,7 +3,6 @@ package utils::curl;
 use strict; use warnings;
 
 our $VERSION    = '0.01';
-our $XS_VERSION = '0.01';
 
 require DynaLoader;
 DynaLoader::bootstrap('utils::curl', $VERSION);
--- lib/utils/curl_constants.pm
+++ lib/utils/curl_constants.pm
@@ -1,7 +1,6 @@
 package utils::curl_constants;
 
 our $VERSION    = '0.01';
-our $XS_VERSION = '0.01';
 
 require DynaLoader;
 DynaLoader::bootstrap(__PACKAGE__, $VERSION);
