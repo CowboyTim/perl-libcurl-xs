2c712200fa4cee7562003bd2da17c87d0aaaf45e
Tim Aerts <aardbeiplantje@gmail.com>
2024-11-28 18:05:51 +0100

    use DynaLoader instead of XSLoader

--- lib/utils/curl.pm
+++ lib/utils/curl.pm
@@ -1,3 +1,14 @@
+package utils::curl;
+
+use strict; use warnings;
+
+our $VERSION = '0.01';
+
+require DynaLoader;
+DynaLoader::bootstrap('utils::curl', $VERSION);
+
+sub dl_load_flags { 0x01 }
+
 package http;
 
 require Exporter;
@@ -6,19 +17,16 @@ our @EXPORT      = qw();
 our @EXPORT_OK   = qw(
 );
 our %EXPORT_TAGS = (all => \@EXPORT_OK);
-our $VERSION     = '0.01';
-our $XS_VERSION  = $VERSION;
 our $AUTOLOAD;
-
-require XSLoader;
-XSLoader::load('utils::curl', $VERSION);
 
 sub AUTOLOAD {
     my (@a_args) = @_;
     my $c_name = $AUTOLOAD;
     $c_name =~ s/.*:://;
     if($c_name =~ m/^curl_version|curl_version_info|curl_getdate|global_(init|cleanup|trace)$/){
-        XSLoader::load('utils::curl_common', $VERSION);
+        no strict 'refs';
+        require utils::curl_common;
+        DynaLoader::bootstrap('utils::curl_common', '0.01');
         unless(UNIVERSAL::can("http",$c_name)){
             my @cl = caller(0);
             die "Undefined subroutine &${cl[0]}::$c_name called at $cl[1] line $cl[2].\n";
@@ -26,7 +34,9 @@ sub AUTOLOAD {
         return &{"http::$c_name"}(@a_args);
     }
     if($c_name =~ m/^(?:CURLE|CURLINFO|CURLPROXY|CURLM|CURLMOPT|CURLMSG)_(?:.*)$/){
-        XSLoader::load('utils::curl_constants', $VERSION);
+        no strict 'refs';
+        require utils::curl_constants;
+        DynaLoader::bootstrap('utils::curl_constants', '0.01');
         unless(UNIVERSAL::can("http",$c_name)){
             my @cl = caller(0);
             die "Undefined subroutine &${cl[0]}::$c_name called at $cl[1] line $cl[2].\n";
@@ -39,7 +49,9 @@ sub AUTOLOAD {
             my $opt = eval {http::curl_easy_option_by_name($opt_name)};
             return $opt->{id} if defined $opt;
         }
-        XSLoader::load('utils::curl_constants', $VERSION);
+        no strict 'refs';
+        require utils::curl_constants;
+        DynaLoader::bootstrap('utils::curl_constants', '0.01');
         unless(UNIVERSAL::can("http",$c_name)){
             my @cl = caller(0);
             die "Undefined subroutine &${cl[0]}::$c_name called at $cl[1] line $cl[2].\n";
@@ -61,7 +73,7 @@ package http::curl::multi;
 sub DESTROY {
     my ($curlm) = @_;
     return unless defined $curlm and defined $$curlm;
-    foreach my $easy_handle (http::curl_multi_get_handles($curlm)){
+    foreach my $easy_handle (eval {http::curl_multi_get_handles($curlm)}){
         http::curl_multi_remove_handle($curlm, $easy_handle);
         http::curl_easy_cleanup($easy_handle);
     }
--- lib/utils/curl_constants.pm
+++ lib/utils/curl_constants.pm
@@ -1,15 +1,10 @@
 package utils::curl_constants;
 
-require Exporter;
-our @ISA = qw(Exporter);
-our @EXPORT      = qw();
-our @EXPORT_OK   = qw(
-);
-our %EXPORT_TAGS = (all => \@EXPORT_OK);
-our $VERSION     = '0.01';
-our $XS_VERSION  = $VERSION;
+our $VERSION = '0.01';
 
-require XSLoader;
-XSLoader::load('utils::curl_constants', $VERSION);
+require DynaLoader;
+DynaLoader::bootstrap(__PACKAGE__, $VERSION);
+
+sub dl_load_flags { 0x01 }
 
 1;
