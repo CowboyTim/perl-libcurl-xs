6bc75e8555ded0c59af17952554b5e747f5eb1c8
Tim Aerts <aardbeiplantje@gmail.com>
2024-12-05 10:18:48 +0100

    build_curl.sh added

---
+++ build_curl.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+./configure \
+    --without-ca-embed \
+    --without-libpsl \
+    --without-libgsasl \
+    --without-libssh2 \
+    --without-libssh \
+    --with-openssl \
+    --without-brotli \
+    --disable-ftp \
+    --disable-tftp \
+    --with-nghttp2 \
+    --without-ldap \
+    --disable-pop3 \
+    --disable-imap \
+    --disable-smb \
+    --disable-smtp \
+    --disable-telnet \
+    --disable-gopher \
+    --disable-rtsp \
+    --disable-dnsshuffle \
+    --enable-get-easy-options \
+    --enable-websockets \
+    --disable-alt-svc \
+    --disable-docs \
+    --disable-dict \
+    --disable-ipfs \
+    --disable-ipns \
+    --disable-mqtt \
+    --disable-file \
+    --enable-ipv6 \
+    --disable-openssl-auto-load-config \
+    --enable-shared \
+    --enable-aws \
+    --disable-unix-sockets \
+    --disable-mime \
+    --disable-form-api \
+    --without-librtmp \
+    --without-apple-idn \
+    --without-winidn \
+    --prefix=/var/tmp/crl
